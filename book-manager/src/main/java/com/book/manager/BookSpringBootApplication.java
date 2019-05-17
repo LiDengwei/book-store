2a807013484696b3310f3b8be381a5b74d615eec
henry <henry@yahoo.com>
2019-05-17 17:23:25 +0800

    初始

--- book-manager/src/main/java/com/book/manager/BookSpringBootApplication.java
+++ book-manager/src/main/java/com/book/manager/BookSpringBootApplication.java
@@ -3,10 +3,13 @@ package com.book.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by henry on 2019/5/17 17:21
+ */
 @SpringBootApplication//这里的MybatisAutoConfiguration.class对应你的数据库框架
-public class BookSpringbootApplication {
+public class BookSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookSpringBootApplication.class, args);
+    }
 }
